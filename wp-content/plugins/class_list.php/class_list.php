3534a04856b8c5c3a0f9eeadff8625d1c67d8309
Willyb15 <william.steen.bryant@gmail.com>
2016-05-25 13:24:52 -0400

    updated php plugin

---
+++ wp-content/plugins/class_list.php/class_list.php
@@ -0,0 +1,97 @@
+<?php
+/**
+* Plugin Name: class list controller
+* Description: This is the custom controller for the Wp-Challenge.
+* Version: 1.0.
+* Author: William Bryant
+*/
+function say_hello_world(){
+	print "Hello, world";
+}
+
+// $theClass = array(
+// 	'Tristan',
+// 	'Joshua',
+// 	'Bogdan',
+// 	'Laz',
+// 	'Rock Man',
+// 	'Will',
+// 	'Curtis',
+// 	'Joe',
+// 	'Koshan',
+// 	'Patrick',
+// 	'The Handsome One',
+// 	'Jeremy'
+// 	);
+
+function getClass(){
+global $wpdb;
+
+$result = $wpdb->get_results('SELECT * FROM class_list');
+
+$class = array();
+
+foreach($result as $row){
+	$class[] = get_object_vars($row);
+}
+print '<pre>';
+print_r($class);
+
+
+// print_r($class);
+// exit;
+// print_r (get_object_vars($result[0]));
+// $encoded_class =json_encode($result);
+// return $result;
+// $theClass = array(
+// 	'Tristan',
+// 	'Joshua',
+// 	'Bogdan',
+// 	'Laz',
+// 	'Rock Man',
+// 	'Will',
+// 	'Curtis',
+// 	'Joe',
+// 	'Koshan',
+// 	'Patrick',
+// 	'The Handsome One',
+// 	'Jeremy'
+// 	);
+
+// return;
+
+// array_pop($class);
+
+
+// $theClass = array(
+// 	'student_one' => 'Tristan',
+// 	'student_two' => 'Joshua',
+// 	'student_three' => 'Bogdan',
+// 	'student_four' => 'Laz',
+// 	'student_five' => 'Rock Man',
+// 	'student_six' => 'Will',
+// 	'student_seven' => 'Curtis',
+// 	'student_eight' => 'Joe',
+// 	'student_nine' => 'Koshan',
+// 	'student_ten' => 'Patrick',
+// 	'student_eleven' => 'The Handsome One',
+// 	'student_twelve' => 'Jeremy'
+// 	);
+
+// $class = array();
+// $class[] = 'Tristan';
+// $class[] = 'Josh';
+// array_push($class, 'Tristan');
+// array_push($class, 'Josh');
+
+// $class - new stdClass;
+// $class->student_one = 'Tristan';
+// $class->student_two = 'Josh';
+
+}
+
+// function select_from_DB(){
+// 	$result = $wpdb->query('SELECT * FROM class_list');
+// 	print $result;
+// 	exit;
+// };
